2864c04a8c78a285e56a0e75bc4cf11cf0ed40eb
SungjaeBae <dunkey2615@gmail.com>
2021-03-31 13:08:00 +0900

    추가 : RepositoryTest.kt 작성

---
+++ data/src/test/java/com/baetory/data/RepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.baetory.data
+
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.Before
+import org.mockito.MockitoAnnotations
+
+abstract class RepositoryTest {
+
+    @Before
+    open fun setup(){
+        MockitoAnnotations.initMocks(this)
+        RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+    }
+
+}
